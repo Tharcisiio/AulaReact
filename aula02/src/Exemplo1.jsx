2f158372e6365f204fe55b8fea6f7c58bf766513
Tharcisiio <souzalimeiratharcisio@gmail.com>
2026-04-28 11:35:34 -0300

    28/04/2026

---
+++ aula02/src/Exemplo1.jsx
@@ -0,0 +1,19 @@
+export default function Exemplo1( {numero1, numero2})
+{
+    let soma = Number(numero1) + Number(numero2);
+
+    return (
+        <div>
+            <p>
+                Número1 : {numero1} <br />
+                Número2 : {numero2} <br />
+                Aa soma é: {soma}
+
+
+            </p>
+        </div>
+    );
+
+
+
+}

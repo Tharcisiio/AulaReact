91fb842324c6fba11135450f12dd8903cb877e91
Tharcisiio <souzalimeiratharcisio@gmail.com>
2026-05-05 11:37:41 -0300

    05/05/2026

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,23 +1,59 @@
 import "./App.css";
 import Exemplo1 from "./Exemplo1";
 
-export default function App() 
-{
+import Exercicio1 from "./Exercicio1";
+
+import Exercicio2 from "./Exercicio2";
+
+import Exercicio3 from "./Exercicio3";
+
+export default function App() {
   return (
     <div>
       <h1>Aula 02 - Criação de Componentes</h1>
 
       <div className="card">
-        <h3>Chamadas para o Componente Exemplo1</h3>
+        <h3>Chamadas para o Componente Exemplo 1</h3>
 
     <Exemplo1 numero1={10} numero2={20} />
+      </div>
+      
+
+      <div className="card">
+        <h3>Exercício 1</h3>
+        <Exercicio1 numero={12} />
+        <Exercicio1 numero={67} />
+        <Exercicio1 numero={-5} />
 
       </div>
 
 
+      <div className="card">
+        <h3>Chamadas para o Componente Exercicio 2</h3>
 
-    </div>
+        
+      <Exercicio2 peso= {60} altura= {80} />
+        
+        </div>
 
+
+        <div className="card">
+              <h3>Chamadas para o Componente Exercicio 3</h3>
+
+              <Exercicio3 nota1= {7} nota2 = {9} />
+
+        </div>
+
+
+
+
+
+      
+
+
+     
+   
+      </div>
 
   )
 }
---
+++ aula02/src/Exercicio3.jsx
@@ -0,0 +1,25 @@
+export default function Exercicio3 ( {nota1,nota2} )
+{
+
+    let media = Number(nota1) + Number(nota2)/2;
+
+
+
+
+
+
+return(
+
+    <div>
+        Nota 1 : {nota1} <br />
+        Nota 2 : {nota2} <br />
+        Media Aritmetica {media}
+    </div>
+
+
+
+
+
+
+)
+}

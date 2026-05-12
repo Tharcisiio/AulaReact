4efbd28a49a1cf7f63372496fa099b135601348f
Tharcisiio <souzalimeiratharcisio@gmail.com>
2026-05-12 11:38:46 -0300

    12/05/2026

--- aula02/src/Exercicio2.jsx
+++ aula02/src/Exercicio2.jsx
@@ -2,7 +2,6 @@ export default function Exercicio2 ( {peso,altura} )
 {
 let imc = Number(peso) / Number(altura)*Number (altura)
 
-
 return(
 
 <div>
--- aula02/src/Exercicio3.jsx
+++ aula02/src/Exercicio3.jsx
@@ -3,11 +3,6 @@ export default function Exercicio3 ( {nota1,nota2} )
 
     let media = Number(nota1) + Number(nota2)/2;
 
-
-
-
-
-
 return(
 
     <div>
